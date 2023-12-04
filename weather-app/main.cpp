d5a73ed521cdd59091be4d7c59e1c5cc32620154
veljkoprodan <veljkoprodan@yahoo.com>
2023-12-04 23:16:17 +0100

    Update CMakeLists.txt

--- weather-app/main.cpp
+++ weather-app/main.cpp
@@ -1,14 +1,10 @@
 #include "MainWindow.h"
-#include "location.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
-
     QApplication a(argc, argv);
-
-    Location l;
     MainWindow w;
     w.show();
     return a.exec();

c3028edd0fa20246bb5de201c91d096d5d9a140c
Vukasin Markovic 312/2019 <skolskiacc@gmail.com>
2023-12-30 01:19:39 +0100

    add more parseGeocodingData test

--- Tests/test_Parser.cpp
+++ Tests/test_Parser.cpp
@@ -55,6 +55,74 @@ TEST_CASE("WeatherData parser"){
 
 TEST_CASE("parseGeocodingData") {
 
+    SECTION("Test with valid JSON data") {
+
+        QJsonArray resultsArray;
+        QJsonObject resultObject1, resultObject2;
+
+        resultObject1["formatted"] = "Belgrade, Serbia";
+        QJsonObject geometryObject1;
+        geometryObject1["lat"] = 44.7866;
+        geometryObject1["lng"] = 20.4489;
+        resultObject1["geometry"] = geometryObject1;
+        QJsonObject componentsObject1;
+        componentsObject1["country"] = "Serbia";
+        resultObject1["components"] = componentsObject1;
+        resultsArray.append(resultObject1);
+
+        resultObject2["formatted"] = "Paris, France";
+        QJsonObject geometryObject2;
+        geometryObject2["lat"] = 48.8566;
+        geometryObject2["lng"] = 2.3522;
+        resultObject2["geometry"] = geometryObject2;
+        QJsonObject componentsObject2;
+        componentsObject2["country"] = "France";
+        resultObject2["components"] = componentsObject2;
+        resultsArray.append(resultObject2);
+
+        QList<GeoLocationData> locations;
+        Parser::parseGeocodingData(resultsArray, locations);
+
+        REQUIRE(locations.size() == 2);
+
+    }
+
+    SECTION("Test with valid JSON data, but missing country") {
+        QJsonArray resultsArray;
+        QJsonObject resultObject;
+
+        resultObject["formatted"] = "Belgrade, Serbia";
+        QJsonObject geometryObject;
+        geometryObject["lat"] = 44.7866;
+        geometryObject["lng"] = 20.4489;
+        resultObject["geometry"] = geometryObject;
+        resultsArray.append(resultObject);
+
+        QList<GeoLocationData> locations;
+        Parser::parseGeocodingData(resultsArray, locations);
+
+        REQUIRE(locations.size() == 1);
+    }
+
+    SECTION("Test with valid JSON data, but missing entire componentsObject") {
+        QJsonArray resultsArray;
+        QJsonObject resultObject;
+
+        resultObject["formatted"] = "Belgrade, Serbia";
+        QJsonObject geometryObject;
+        geometryObject["lat"] = 44.7866;
+        geometryObject["lng"] = 20.4489;
+        resultObject["geometry"] = geometryObject;
+        QJsonObject componentsObject;
+        resultObject["components"] = componentsObject;
+        resultsArray.append(resultObject);
+
+        QList<GeoLocationData> locations;
+        Parser::parseGeocodingData(resultsArray, locations);
+
+        REQUIRE(locations.size() == 1);
+    }
+
     SECTION("Test with invalid JSON data, missing required fields") {
         QJsonArray resultsArray;
         QJsonObject invalidResultObject;
@@ -64,9 +132,46 @@ TEST_CASE("parseGeocodingData") {
         QList<GeoLocationData> locations;
         Parser::parseGeocodingData(resultsArray, locations);
 
-        //onda je prazna
         REQUIRE(locations.isEmpty());
     }
+
+    SECTION("Test with invalid JSON data, missing lat and lng") {
+        QJsonArray resultsArray;
+        QJsonObject resultObject;
+
+        resultObject["formatted"] = "Belgrade, Serbia";
+        QJsonObject geometryObject;
+        resultObject["geometry"] = geometryObject;
+        QJsonObject componentsObject;
+        componentsObject["country"] = "Serbia";
+        resultObject["components"] = componentsObject;
+        resultsArray.append(resultObject);
+
+        QList<GeoLocationData> locations;
+        Parser::parseGeocodingData(resultsArray, locations);
+
+        REQUIRE(locations.isEmpty());
+    }
+
+    SECTION("Test with invalid JSON data, missing formatted") {
+        QJsonArray resultsArray;
+        QJsonObject resultObject;
+
+        QJsonObject geometryObject;
+        geometryObject["lat"] = 44.7866;
+        geometryObject["lng"] = 20.4489;
+        resultObject["geometry"] = geometryObject;
+        QJsonObject componentsObject;
+        componentsObject["country"] = "Serbia";
+        resultObject["components"] = componentsObject;
+        resultsArray.append(resultObject);
+
+        QList<GeoLocationData> locations;
+        Parser::parseGeocodingData(resultsArray, locations);
+
+        REQUIRE(locations.isEmpty());
+    }
+
 }
 
 

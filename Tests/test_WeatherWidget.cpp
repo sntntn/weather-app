5c2b8bfed13bc4fb02b6e3f04d0f48c09e8da3fd
floxinoxinihiliplification <lucija.kecic@gmail.com>
2023-12-30 20:06:20 +0100

    DetailedWeatherAPI, ErrorWidget and WeatherWidget tests added

---
+++ Tests/test_WeatherWidget.cpp
@@ -0,0 +1,28 @@
+#include "GeoLocationData.h"
+#include "catch.hpp"
+
+#include "WeatherWidget.h"
+#include "qgeocoordinate.h"
+#include "qtimezone.h"
+#include "WeatherData.h"
+
+TEST_CASE("Test WeatherWidget initialization")
+{
+    SECTION("WeatherWidget Constructor Test")
+    {
+        // Arrange
+        GeoLocationData location("Belgrade", "Belgrade", QGeoCoordinate(44.8125, 20.4375), "Serbia");
+        QTimeZone timezone("Europe/Belgrade");
+
+        QSharedPointer<WeatherData> sharedWeatherData = QSharedPointer<WeatherData>::create(
+            location, 25, 30, 20, 65, true, timezone);
+
+        // Act
+        WeatherWidget* weatherWidget = new WeatherWidget(sharedWeatherData, nullptr);
+
+        // Assert
+        REQUIRE(weatherWidget != nullptr);
+    }
+}
+
+

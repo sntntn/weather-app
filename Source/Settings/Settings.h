f12e850f7795d0f739ac62aa93a3b3db56856eec
MilenkovicMaja <milenkovicmaja16@gmail.com>
2023-12-30 02:39:37 +0100

    Add tests for Settings class

--- Source/Settings/Settings.h
+++ Source/Settings/Settings.h
@@ -57,7 +57,7 @@ public:
     };
     Q_ENUM(PrecipitationUnit)
 
-    inline QList<GeoLocationData> &savedLocations()
+    inline QList<GeoLocationData>& savedLocations()
     {
         return m_savedLocations;
     }
